980941a503e1a047da7255ad201787753da683c8
Huzaifa Qamer <huzaifaqamer@gmail.com>
2020-07-30 18:57:13 +0500

    Added token model

--- api/auth/models.py
+++ api/auth/models.py
@@ -13,3 +13,10 @@ class User(db.Model, UserMixin):
     # User information
     first_name = db.Column(db.String(100))
     last_name = db.Column(db.String(100))
+
+
+class Token(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    auth_token = db.Column(db.String(255), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship(User, backref=db.backref('token', lazy=True, uselist=False))
