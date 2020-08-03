5d71081eb0d704fa342a98f28a96c0a57b64dad5
Huzaifa Qamer <huzaifaqamer@gmail.com>
2020-08-03 15:59:25 +0500

    token_required decorator

---
+++ api/common.py
@@ -0,0 +1,41 @@
+from functools import wraps
+
+from flask import request
+
+from api.auth.models import User, Token
+
+
+def __get_user_from_token(token):
+    token = Token.query.filter_by(auth_token=token).join(User).first()
+    if token:
+        return token.user
+    
+    return None
+
+
+def token_required(func):
+    """
+    check request header for presence of 'Authorization'
+    header and test the recieved token for its validity.
+    If a user is found then adds a user attribute to request object
+    """
+    @wraps(func)
+    def inner_func(*args, **kwargs):
+        token = request.headers.get('Authorization')
+        if token:
+            try:
+                splitted_token = token.split()
+                if splitted_token[0] != 'Token':
+                    return {'error': f"expected 'Token' but got '{splitted_token[0]}' in 'Authorization' header"}, 400
+                
+                user = __get_user_from_token(splitted_token[1])
+                if user:
+                    request.user = user
+                    return func(*args, **kwargs)
+
+                return {'error': 'Provided token is invalid'}, 401
+            except IndexError:
+                pass
+        return {'error': 'Authorization token not provided'}, 401
+
+    return inner_func
